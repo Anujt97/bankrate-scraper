8045e1527b77ef9b736781ca90c4772d4bc48cc3
Anujkumar Tiwari <93044865+Anujt97@users.noreply.github.com>
2025-07-17 14:33:06 +0530

    added with user agent

--- bankratescrapping/settings.py
+++ bankratescrapping/settings.py
@@ -1,3 +1,4 @@
 BOT_NAME = 'bankratescrapping'
 SPIDER_MODULES = ['bankratescrapping.spiders']
 NEWSPIDER_MODULE = 'bankratescrapping.spiders'
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
