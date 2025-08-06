1b3d4ecfc456fcc718b13e92a5be1747dd8fd794
Anujkumar Tiwari <93044865+Anujt97@users.noreply.github.com>
2025-08-06 10:24:10 +0530

    Updated logging with setting.py and yml is been updated

--- bankratescrapping/settings.py
+++ bankratescrapping/settings.py
@@ -2,3 +2,7 @@ BOT_NAME = 'bankratescrapping'
 SPIDER_MODULES = ['bankratescrapping.spiders']
 NEWSPIDER_MODULE = 'bankratescrapping.spiders'
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+LOG_ENABLED = True
+LOG_LEVEL = 'INFO'  # Options: 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
+LOG_FILE = 'scrapy_log.log'
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
